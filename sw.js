b5bfc0365bae24b7a82bcce3e2dbe8d66bdc6488
esparragoza645-coder <esparragoza645@gmail.com>
2026-03-15 20:30:24 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'mis-gastos-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
